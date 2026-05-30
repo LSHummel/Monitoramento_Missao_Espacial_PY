d6305ec3ffb14e69792f60f06cecb8d310953cee
LSHummel <lshummel13@gmail.com>
2026-05-30 15:49:36 -0300

    criação do menu

---
+++ Monitoramento_Energetico_Espacial.py
@@ -0,0 +1,31 @@
+
+while True:
+
+    print("====================")
+    print("       MENU")
+    print("====================")
+    print("1 - Insira os dados")
+    print("2 - Visualisar status")
+    print("3 - Executar analise")
+    print("4 - Relatório da missão")
+    print("5 - SAIR")
+    opcao = int(input("Digite o número da opcão que deseja: "))
+
+
+    if opcao == 1:
+        print("opcao 1")
+
+    elif opcao == 2:
+        print("opcao 2")
+
+    elif opcao == 3:
+        print("opcao 3")
+
+    elif opcao == 4:
+        print("opcao 4")
+
+    elif opcao == 5:
+        print("FIM DA SIMULAÇÂO")
+        break
+    else:
+        print("Número inserido invalido!")
